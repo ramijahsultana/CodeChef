1f3e51034b3ade984ed87d4450d644731c9aac5f
Rafa <ramijahsultana@gmail.com>
2026-09-02 19:42:43 +0600

    CC

---
+++ 500 to 1000 Difficulty/Minimum_Cars_required.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int n;
+	    cin >> n;
+	    if(n <= 4)
+	    {
+	        cout << "1\n";
+	    }
+	    else if(n%4 != 0)
+	    {
+	        cout << (n/4)+1 << "\n";
+	    }
+	    else
+	        cout << n/4 << "\n";
+	    
+	}
+
+}
