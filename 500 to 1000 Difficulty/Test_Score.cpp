80a0dd849dc8a04f728091a132a671ba5cb71c26
Rafa <ramijahsultana@gmail.com>
2026-09-03 21:44:16 +0600

    CC

---
+++ 500 to 1000 Difficulty/Test_Score.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int n,x,y;
+	    cin >> n >> x >> y;
+	   if (y % x == 0 && (y / x) <= n) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
+        }
+	   
+	}
+
+}
