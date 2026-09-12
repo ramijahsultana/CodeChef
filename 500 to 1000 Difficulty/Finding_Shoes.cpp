945f803e365e3839ad3137588306c62da1d1ce02
Rafa <ramijahsultana@gmail.com>
2026-09-12 20:06:09 +0600

    CC

---
+++ 500 to 1000 Difficulty/Finding_Shoes.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int n, m;
+	    cin >> n >> m;
+	    int v = n*2;
+	    if(n<m)
+	        cout << n << "\n";
+	    else
+	        cout << v-m << "\n";
+	    
+	}
+	
+}
